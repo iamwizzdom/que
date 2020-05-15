de174693e3f02e5cbfd78954679057c5075a1756
iamwizzdom <wisdomemenike70@gmail.com>
2020-05-15 08:46:59 +0100

    Route registration

--- app/app.route.php
+++ app/app.route.php
@@ -8,41 +8,13 @@ require 'app.settings.php';
 Route::register()->web(function (RouteEntry $entry) {
     $entry->setUri('/');
     $entry->setRequireCSRFAuth(false);
-    $entry->setModule('View');
+    $entry->setModule(View::class);
 });
 
 Route::register()->api(function (RouteEntry $entry) {
     $entry->setUri('api');
     $entry->setRequireCSRFAuth(false);
-    $entry->setModule('App');
+    $entry->setModule(App::class);
 });
-
-//Route::register()->group('profile', function (string $prefix) {
-//
-//    return [
-//        function(RouteEntry $entry) {
-//            $entry->setType('web');
-//            $entry->setUri('/');
-//            $entry->setRequireLogIn(true);
-//            $entry->setModule('module\profile\View');
-//            $entry->setImplement(RouteImplementEnum::IMPLEMENT_PAGE);
-//        },
-//        function(RouteEntry $entry) {
-//            $entry->setType('web');
-//            $entry->setUri('bank');
-//            $entry->setRequireLogIn(true);
-//            $entry->setModule('module\profile\Bank');
-//            $entry->setImplement(RouteImplementEnum::IMPLEMENT_PAGE);
-//        },
-//        function(RouteEntry $entry) {
-//            $entry->setType('web');
-//            $entry->setUri('message');
-//            $entry->setRequireLogIn(true);
-//            $entry->setModule('module\profile\Message');
-//            $entry->setImplement(RouteImplementEnum::IMPLEMENT_PAGE);
-//        }
-//    ];
-//
-//});
 
 Route::init();
